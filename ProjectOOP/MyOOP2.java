e7052fad814ce0bf3bfb564ee13a07a21d0f4fb3
decoy92 <83627908+decoy92@users.noreply.github.com>
2021-09-15 23:32:51 +0900

    Add files via upload

--- ProjectOOP/MyOOP2.java
+++ ProjectOOP/MyOOP2.java
@@ -1,16 +1,3 @@
-class Print{
-	public static String delimiter = "";
-	public static void A() {		// 메소드 내용
-		System.out.println(delimiter);
-		System.out.println("A");
-		System.out.println("A");
-	}
-	public static void B() {		// 메소드 내용
-		System.out.println(delimiter);
-		System.out.println("B");
-		System.out.println("B");
-	}
-}
 public class MyOOP2 {
 	public static void main(String[] args) {
 		Print.delimiter = "----";
---
+++ ProjectOOP/Print.java
@@ -0,0 +1,16 @@
+
+class Print{
+	public static String delimiter = "";
+	
+	public static void A() {		// 메소드 내용
+		System.out.println(delimiter);
+		System.out.println("A");
+		System.out.println("A");
+	}
+	
+	public static void B() {		// 메소드 내용
+		System.out.println(delimiter);
+		System.out.println("B");
+		System.out.println("B");
+	}
+}
