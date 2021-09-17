98279bfd7a52c1d981b1a37cb99f1e3acca95b4a
decoy92 <83627908+decoy92@users.noreply.github.com>
2021-09-17 23:55:56 +0900

    Add files via upload

--- ProjectOOP/StaticApp.java
+++ ProjectOOP/StaticApp.java
@@ -1,8 +1,24 @@
-
+class Foo{
+	public static String classVar = "I class var";
+	public String instanceVar = "I instance var";
+	public static void classMethod() {
+		System.out.println(classVar); // OK
+//		System.out.println(intanceVar); // Error	
+	}
+	public void instanceMethod() {
+		System.out.println(classVar); // OK
+		System.out.println(instanceVar); // OK
+	}
+}
 public class StaticApp {
 
 	public static void main(String[] args) {
+		System.out.println(Foo.classVar); // 클래스 접근가능
+//		System.out.println(Foo.instanceVar); // 인스턴스 접근 불가
+		Foo.classMethod(); // OK
+//		Foo.instanceMethod(); // Error
 		
+		Foo f1 = new Foo();
 		
 		
 	}
