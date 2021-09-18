c94e97efd6fea77ca822805012acae23403e2cac
decoy92 <83627908+decoy92@users.noreply.github.com>
2021-09-18 23:47:15 +0900

    Add files via upload

--- ProjectOOP/MyOOP3.java
+++ ProjectOOP/MyOOP3.java
@@ -1,14 +1,12 @@
 public class MyOOP3 {
 	public static void main(String[] args) {
-		Print p1 = new Print(); 
-		p1.delimiter = "----";
+		Print p1 = new Print("----"); 
 		p1.A();
 		p1.A();
 		p1.B();
 		p1.B();
 		
-		Print p2 = new Print(); 
-		p2.delimiter = "****";
+		Print p2 = new Print("****"); 
 		p2.A();
 		p2.A();
 		p2.B();
--- ProjectOOP/Print.java
+++ ProjectOOP/Print.java
@@ -1,14 +1,16 @@
 class Print{
 	public String delimiter = "";
-	
+	public Print(String delimiter) {
+		this.delimiter = delimiter;
+	}
 	public void A() {		// 메소드 내용
-		System.out.println(delimiter);
+		System.out.println(this.delimiter);
 		System.out.println("A");
 		System.out.println("A");
 	}
 	
 	public void B() {		// 메소드 내용
-		System.out.println(delimiter);
+		System.out.println(this.delimiter);
 		System.out.println("B");
 		System.out.println("B");
 	}
