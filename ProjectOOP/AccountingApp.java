5bef77e50aee8aad3c93c247b9c8947bc1100528
Heisenbug <83627908+decoy92@users.noreply.github.com>
2021-09-20 23:56:14 +0900

    Add files via upload

--- ProjectOOP/AccountingApp.java
+++ ProjectOOP/AccountingApp.java
@@ -1,7 +1,7 @@
 class Accounting{
-    public static double valueOfSupply;
+    public double valueOfSupply;
     public static double vatRate = 0.1;
-    public static double getVAT() {
+    public double getVAT() {
         return valueOfSupply * vatRate;
     }
     public static double getTotal() {
@@ -10,10 +10,36 @@ class Accounting{
 }
 public class AccountingApp {
     public static void main(String[] args) {
-    	Accounting.valueOfSupply = 10000.0;
-        System.out.println("Value of supply : " + Accounting.valueOfSupply);
-        System.out.println("VAT : " + Accounting.getVAT());
-        System.out.println("Total : " + Accounting.getTotal());
- 
+    	Accounting a1 = new Accounting();
+    	a1.valueOfSupply = 10000.0;
+    	
+    	Accounting a2 = new Accounting();
+    	a2.valueOfSupply = 20000.0;
+    	
+    	System.out.println("Value of supply : " + a1.valueOfSupply);
+    	System.out.println("Value of supply : " + a2.valueOfSupply);
+    	
+    	System.out.println("VAT : " + a1.getVAT());
+    	System.out.println("VAT : " + a1.getVAT());
+    	
+    	System.out.println("Total : " + a1.getTotal());
+    	System.out.println("Total : " + a2.getTotal());
+    	
+    	
+//    	Accounting.valueOfSupply = 10000.0;
+//        System.out.println("Value of supply : " + Accounting.valueOfSupply);
+//        Accounting.valueOfSupply = 20000.0;
+//        System.out.println("Value of supply : " + Accounting.valueOfSupply);
+//        
+//        Accounting.valueOfSupply = 10000.0;
+//        System.out.println("VAT : " + Accounting.getVAT());
+//        Accounting.valueOfSupply = 20000.0;
+//        System.out.println("VAT : " + Accounting.getVAT());
+//        
+//        Accounting.valueOfSupply = 10000.0;
+//        System.out.println("Total : " + Accounting.getTotal());
+//        Accounting.valueOfSupply = 20000.0;
+//        System.out.println("Total : " + Accounting.getTotal());
+        
     }
 }
